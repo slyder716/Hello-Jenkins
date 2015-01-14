7182caa8bb367b2e2bbd506f98bb08be5ccd3013
slyder716 <jon_saunders@comcast.net>
2015-01-13 22:27:45 -0800

    Add html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('<hr/>Blah<hr/>');
+  res.send('<hr>Blah<hr>');
 });
  
 app.listen(process.env.PORT || 5000);
