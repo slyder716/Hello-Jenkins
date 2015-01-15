5762301c5dbe0ab7a9fb2ae74d4fffd2988bfd3f
slyder716 <jon_saunders@comcast.net>
2015-01-14 19:41:55 -0800

    Blah Blah

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Blah Blah Blah');
+  res.send('Blah Blah');
 });
  
 app.listen(process.env.PORT || 5000);
