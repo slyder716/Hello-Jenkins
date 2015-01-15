61471e61e39d317c6cf22073f9c2e972928f51ff
slyder716 <jon_saunders@comcast.net>
2015-01-14 21:12:37 -0800

    html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('Hello Jenkins!');
+  res.send('<html><body><hr>Some Text<hr></body></html>');
 });
  
 app.listen(process.env.PORT || 5000);
