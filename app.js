ce880e364fea2f502037987e8245c42f2ad4a624
slyder716 <jon_saunders@comcast.net>
2015-01-23 16:12:40 -0700

    html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('<html><body><hr>Some Text<hr></body></html>');
+  res.send('Hello World');
 });
  
 app.listen(process.env.PORT || 5000);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('<html><body><hr>Some Text<hr></body></html>', done);
+    request(app).get('/').expect('Hello World', done);
   });
 });
